907d6c3dee55234d08dac6664e8ed74dd2835aff
cmsbuild <>
2009-05-29 18:37:35 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- EcalBarrelMonitorTasks-V00-09-28'.
    Sprout from master 2009-05-29 18:37:27 UTC Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch> ''
    Cherrypick from master 2008-12-03 15:46:39 UTC Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch> '':
        interface/EBRawDataTask.h
        python/EBRawDataTask_cfi.py
        src/EBRawDataTask.cc
    Cherrypick from master 2009-02-26 14:54:45 UTC Emanuele Di Marco <sha1-7725c4b6a787836a48e2e74f4d20b6a79f483406@cern.ch> 'adding the PFG cluster plots in the online DQM processing':
        BuildFile
        src/EBClusterTask.cc

--- python/EBRawDataTask_cfi.py
+++ python/EBRawDataTask_cfi.py
@@ -5,5 +5,6 @@ ecalBarrelRawDataTask = cms.EDAnalyzer("EBRawDataTask",
     enableCleanup = cms.untracked.bool(False),
     mergeRuns = cms.untracked.bool(False),
     FEDRawDataCollection = cms.InputTag("source"),
-    EcalRawDataCollection = cms.InputTag("ecalEBunpacker")
+    EcalRawDataCollection = cms.InputTag("ecalEBunpacker"),
+    GTEvmSource = cms.InputTag("l1GtEvmUnpack")
 )
