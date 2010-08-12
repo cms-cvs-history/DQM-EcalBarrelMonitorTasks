39792af4ed67cb71b8bfb1c08c028fca9d7b4063
cmsbuild <>
2010-08-12 19:42:29 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- EcalBarrelMonitorTasks-V00-17-35-DB'.
    Sprout from DQM-EcalBarrelMonitorTasks-old-masking 2010-08-12 19:42:25 UTC Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch> ''
    Cherrypick from DQM-EcalBarrelMonitorTasks-old-masking 2010-08-03 15:53:37 UTC cmsbuild 'This commit was manufactured by cvs2git to create branch 'DQM-':
        interface/EBDataCertificationTask.h
        src/EBDataCertificationTask.cc
    Cherrypick from master 2010-03-27 20:07:58 UTC Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch> '':
        interface/EBDaqInfoTask.h
        interface/EBDcsInfoTask.h
        python/EBDaqInfoTask_cfi.py
        python/EBDcsInfoTask_cfi.py
        src/EBDaqInfoTask.cc
        src/EBDcsInfoTask.cc
    Cherrypick from DQM-EcalBarrelMonitorTasks-noRunInfo 2010-07-26 11:07:29 UTC Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch> '':
        BuildFile.xml

--- interface/EBDaqInfoTask.h
+++ interface/EBDaqInfoTask.h
@@ -1,15 +1,6 @@
 #ifndef EBDaqInfoTask_h
 #define EBDaqInfoTask_h
 
-/*
- * \file EBDaqInfoTask.h
- *
- * $Date: 2010/08/08 08:56:00 $
- * $Revision: 1.4 $
- * \author E. Di Marco
- *
-*/
-
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
@@ -41,12 +32,6 @@ void beginLuminosityBlock(const edm::LuminosityBlock& lumiBlock, const  edm::Eve
 /// EndLuminosityBlock
 void endLuminosityBlock(const edm::LuminosityBlock&  lumiBlock, const  edm::EventSetup& iSetup);
 
-/// BeginRun
-void beginRun(const edm::Run & r, const edm::EventSetup & c);
-
-/// EndRun
-void endRun(const edm::Run & r, const edm::EventSetup & c);
-
 /// Reset
 void reset(void);
 
@@ -54,8 +39,6 @@ void reset(void);
 void cleanup(void);
   
 private:
-
-void fillMonitorElements(int ready[72][34]);
   
 DQMStore* dqmStore_;
 
@@ -69,8 +52,8 @@ MonitorElement* meEBDaqFraction_;
 MonitorElement* meEBDaqActive_[36];
 MonitorElement* meEBDaqActiveMap_;
 
-int readyRun[72][34];
-int readyLumi[72][34];
+int EBFedRangeMin_;
+int EBFedRangeMax_;
 
 };
 
