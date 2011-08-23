2eb7e438ab2920796dab000a47d1fcd00e0d6b1f
Yutaro Iiyama <sha1-b63dc01a642d380a16da36b4e834787802efdeb4@cern.ch>
2011-08-23 00:31:22 +0000

    *** empty log message ***

--- interface/EBTimingTask.h
+++ interface/EBTimingTask.h
@@ -4,8 +4,8 @@
 /*
  * \file EBTimingTask.h
  *
- * $Date: 2011/06/27 09:08:17 $
- * $Revision: 1.16.14.1 $
+ * $Date: 2011/06/27 10:33:50 $
+ * $Revision: 1.16.14.2 $
  * \author G. Della Ricca
  *
 */
@@ -27,7 +27,7 @@ EBTimingTask(const edm::ParameterSet& ps);
 /// Destructor
 virtual ~EBTimingTask();
 
- static const float shiftProf2D;
+static const float shiftProf2D_;
 
 protected:
 
@@ -67,6 +67,8 @@ bool enableCleanup_;
 
 bool mergeRuns_;
 
+float energyThreshold_;
+
 edm::InputTag EcalRawDataCollection_;
 edm::InputTag EcalRecHitCollection_;
 
@@ -82,6 +84,6 @@ bool init_;
 
 };
 
-const float EBTimingTask::shiftProf2D = 50.;
+const float EBTimingTask::shiftProf2D_ = 50.;
 
 #endif
