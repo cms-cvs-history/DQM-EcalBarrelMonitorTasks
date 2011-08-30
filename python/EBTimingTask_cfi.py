3cddf446b72fcd3332084529448617009aaa5589
Yutaro Iiyama <sha1-b63dc01a642d380a16da36b4e834787802efdeb4@cern.ch>
2011-08-30 08:42:27 +0000

    new parameter energyThreshold

--- python/EBTimingTask_cfi.py
+++ python/EBTimingTask_cfi.py
@@ -4,7 +4,8 @@ ecalBarrelTimingTask = cms.EDAnalyzer("EBTimingTask",
     prefixME = cms.untracked.string('EcalBarrel'),
     enableCleanup = cms.untracked.bool(False),
     mergeRuns = cms.untracked.bool(False),
+    energyThreshold = cms.untracked.double(1.0),
     EcalRawDataCollection = cms.InputTag("ecalEBunpacker"),
-    EcalRecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEB")
+    EcalRecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEB"),
 )
 
